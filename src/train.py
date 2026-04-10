b6cdabed2cd26f7f235097ead8b02b5f638723eb
Deyved Antonio <deyved.antonio@gmail.com>
2026-04-10 17:02:43 +0000

    feat: Primeiro Pipeline de Machine Learn

---
+++ src/train.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+import json
+import os
+
+
+train = pd.read_csv('data/processed/train.csv')
+
+X_train = train.drop('Churn', axis=1)
+y_train = train['Churn']
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+os.makedirs('models', exist_ok=True)
+with open('models/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+y_pred = model.predict(X_train)
+acc = accuracy_score(y_train, y_pred)
+
+with open("metrics.json", "w") as f:
+    json.dump({"accuracy": acc}, f, indent=2)
